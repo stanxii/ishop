c1b0573e4e65e34bc8b09d4dc63bbf667c918eb7
undrizzle <undrizzle@gmail.com>
2015-01-16 16:28:59 +0800

    fix edit-profile

--- assets/app/js/scripts.js
+++ assets/app/js/scripts.js
@@ -22,4 +22,25 @@ $(".nav-invite").click(function () {
   $(".nav-problem").removeClass("current-menu-item");
   $(".nav-coder").removeClass("current-menu-item");
 });
+// 上传头像
+$("#upload-btn").click(function(){
+  $("#file-choose").click();
+});
 
+$('#file-choose').fileupload({
+  url: '/upload_avatar',
+  dataType: 'json',
+  start: function(){
+    $(".img-button #upload-btn").text("图片上传中...");
+  },
+  done: function (e, data) {
+    $(".img-tainer img").attr("src", data.result.logo);
+  },
+  error: function(){
+    alert("图片上传失败，请稍后再试");
+  },
+  complete: function(){
+    $(".img-button #upload-btn").text("本地上传");
+  }
+
+});
